d71d4e7c98c9e6cbbe2014e2a702cd1d967c24dc
JANTECH2004 <janamuniraj06@gmail.com>
2026-06-11 15:03:32 +0530

    Initialized driver and builder but not wait and select

--- QEFST/src/test/java/Activity/Base.java
+++ QEFST/src/test/java/Activity/Base.java
@@ -1,14 +1,15 @@
 package Activity;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Base {
 	public void init(){
-		WebDriver driver;
-		Actions builder;
+		WebDriver driver=new FirefoxDriver();
+		Actions builder=new Actions(driver);
 		WebDriverWait wait;
 		Select select;
 	}
