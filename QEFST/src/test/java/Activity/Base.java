61153a71f6bd52bc24e062b6b5f0a45e6bba8cdf
JANTECH2004 <janamuniraj06@gmail.com>
2026-06-11 14:58:21 +0530

    Added base class, if you want extend and use it

---
+++ QEFST/src/test/java/Activity/Base.java
@@ -0,0 +1,15 @@
+package Activity;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Base {
+	public void init(){
+		WebDriver driver;
+		Actions builder;
+		WebDriverWait wait;
+		Select select;
+	}
+}
