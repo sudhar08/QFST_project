0c063d5085514484686d931ff1e42dcec11e8163
Sudharsanan <sudharsanan080@gmail.com>
2026-06-12 14:54:28 +0530

    checking

---
+++ QEFST/src/test/java/Activity/Activity9.java
@@ -0,0 +1,47 @@
+package Activity;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+public class Activity9  extends Base{
+	@Test
+	public void checking_Leads() {
+		driver.get("http://crm.local:3050/#/home");
+		
+		WebElement element = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[text()='Leads']"))));
+		builder.moveToElement(element).perform();
+		
+		
+		WebElement viewlead =    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[text()='View Leads']"))));
+		viewlead.click();
+		
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//scrm-module-title[normalize-space()='LEADS']"))));
+		
+		List<WebElement> colmunname = driver.findElements(By.xpath("//table[contains(@class ,'list-view-table')]//tr/td[3]"));
+		List<WebElement> colmunuser = driver.findElements(By.xpath("//table[contains(@class ,'list-view-table')]//tr/td[8]"));
+		
+		for(WebElement cell : colmunname) {
+		    System.out.println(cell.getText());
+		}
+		
+		for(WebElement cell : colmunuser) {
+		    System.out.println(cell.getText());
+		}
+		
+		
+		
+
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		// close the browser
+		driver.quit();
+	}
+	
+}
--- QEFST/src/test/java/Activity/Activity_3.java
+++ QEFST/src/test/java/Activity/Activity_3.java
@@ -1,6 +1,7 @@
 package Activity;
 
 import org.openqa.selenium.By;
+import static org.testng.Assert.assertEquals;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
@@ -17,6 +18,7 @@ public class Activity_3 extends Base {
 
 	String message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@data-target='.copyright-suitecrm']"))).getText();
 	System.out.println(message);
+	assertEquals(message,"© Supercharged by SuiteCRM");
 	}
 	
 	// @AfterTest
